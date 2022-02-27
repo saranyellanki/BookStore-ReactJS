ea3225fc24707f6e62b0e7c48bbe1aa80b93cc39
saranyellanki <saran.yellanki12@gmail.com>
2022-02-27 17:32:17 +0530

    [Saran] Added: header component to set search and logo

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,73 @@
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import SearchIcon from '@mui/icons-material/Search';
+import education from '../../assets/education.svg';
+import InputBase from '@mui/material/InputBase';
+import './Header.scss';
+import { PersonOutlined, ShoppingCartOutlined } from "@mui/icons-material";
+
+const Header = () => {
+  return (<>
+    <div className='header'>
+      <img className='image-header' src={education} alt="img" />
+      <span className='image-title'>Bookstore</span>
+      <div className="search">
+        <Search>
+          <SearchIconWrapper><SearchIcon style={{fontSize:'medium', color:'#9D9D9D'}} /></SearchIconWrapper>
+          <StyledInputBase
+            placeholder="Search..."
+            inputProps={{ 'aria-label': 'search' }}
+          />
+        </Search>
+      </div>
+      <div className="person-details">
+        <PersonOutlined style={{color:'#FFFFFF'}} />
+        <span className="person-name">{localStorage.getItem('fullName').split(' ')[0]}</span>
+      </div>
+      <div className="cart-details">
+        <ShoppingCartOutlined style={{color:'#FFFFFF'}} />
+        <span className="person-name">Cart</span>
+      </div>
+    </div>
+  </>
+  );
+}
+
+const Search = styled('div')(({ theme }) => ({
+  position: 'relative',
+  borderRadius: '3px',
+  backgroundColor: '#FCFCFC',
+  width: '490px',
+  height: '33px',
+  display: 'flex',
+  alignItems: 'center',
+  [theme.breakpoints.up('sm')]: {
+    marginLeft: theme.spacing(15),
+  },
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+  padding: theme.spacing(0, 1),
+  height: '12.93px',
+  position: 'absolute',
+  pointerEvents: 'none',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: 'inherit',
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 1),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(3)})`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    fontFamily: 'Roboto',
+    fontSize: '12px',
+    paddingTop: '13px',
+    [theme.breakpoints.up('md')]: {
+      width: '20ch',
+    },
+  },
+}));
+
+export default Header;
