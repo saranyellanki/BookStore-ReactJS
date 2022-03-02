fab777887d9130081c4b07024d690f7c9874e461
saranyellanki <saran.yellanki12@gmail.com>
2022-03-02 21:22:20 +0530

    [Saran] Added: Cart services to connect db

---
+++ src/services/CartService.js
@@ -0,0 +1,27 @@
+import AxiosService from "./AxiosService";
+
+const axiosService = new AxiosService();
+
+let baseURL = 'http://localhost:5050/api/v1/bookstore_user';
+
+const header = {
+  headers : {
+    token: localStorage.getItem("token")
+  }
+}
+
+class CartService {
+  addToCart(data){
+    return axiosService.post(`${baseURL}/add_cart_item/${data._id}`,'',header);
+  }
+
+  getCart(){
+    return axiosService.get(`${baseURL}/get_cart_items`,header);
+  }
+
+  deleteItem(data){
+    return axiosService.delete(`${baseURL}/remove_cart_item/${data}`,header)
+  }
+}
+
+export default CartService;
