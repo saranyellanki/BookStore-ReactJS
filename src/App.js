84f10a5634fa58f072d402f41ae4ccd622135526
saranyellanki <saran.yellanki12@gmail.com>
2022-03-02 17:48:07 +0530

    [Saran] Refactored: to add cart path

--- src/App.js
+++ src/App.js
@@ -2,17 +2,19 @@ import './App.css';
 import Homepage from './pages/homepage/Homepage';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Dashboard from './pages/dashboard/Dashboard';
+import Cart from './pages/cart/Cart';
 
 function App() {
   return (
-    <div>
+    <>
       <Router>
         <Routes>
           <Route path='' element={<Homepage />} />
           <Route path='/dashboard' element={<Dashboard />} />
+          <Route path='/cart' element={<Cart />} />
         </Routes>
       </Router>
-    </div>
+    </>
   );
 }
 
