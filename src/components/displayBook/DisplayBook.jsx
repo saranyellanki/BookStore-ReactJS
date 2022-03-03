5996c35f1230b402f1a61fc797833310285a78d0
saranyellanki <saran.yellanki12@gmail.com>
2022-03-03 20:57:24 +0530

    [Saran] Refactored: to implement cart as central state redux

--- src/components/displayBook/DisplayBook.jsx
+++ src/components/displayBook/DisplayBook.jsx
@@ -3,6 +3,9 @@ import './DisplayBook.scss';
 import bookImg from '../../assets/Image 8.png';
 import { Button } from '@mui/material';
 import CartService from '../../services/CartService';
+import { useDispatch, useSelector } from 'react-redux';
+import { getCart } from '../../redux/actions/bookActions';
+
 
 const cartService = new CartService();
 
@@ -10,67 +13,94 @@ const DisplayBook = (props) => {
 
   const [addCart, setAddCart] = React.useState(true);
 
-  const addToCart = () => {
-    setAddCart(false);
-    let data = {
-      "_id": props.arrBook._id
-    }
-    cartService.addToCart(data)
-    .then((res)=> {
-      console.log(res);
-      props.getCart();
-    }).catch((err) => {
-      console.log(err);
-    })
+  const dispatch = useDispatch();
+  const cart = useSelector((state) => state.getCart)
+
+  React.useEffect(() => {
+    getCartItems();
+  }, [])
+
+  const getCartItems = () => {
+    cartService.getCart()
+      .then((res) => {
+        // console.log(res.data.data);
+        dispatch(getCart(res.data.data.book));
+      }).catch((err) => {
+        console.log(err);
+      })
   }
 
-  let image = props.arrBook.bookImage ? props.arrBook.bookImage : bookImg
+  const addToCart = () => {
+    let data = {
+      "_id": props.book._id
+    }
+    cartService.addToCart(data)
+      .then((res) => {
+        // console.log(res);
+        setAddCart(false);
+        dispatch(getCart(res.data.data.book))
+      }).catch((err) => {
+        console.log(err);
+      })
+  }
+
+  const button = () => {
+    const book = cart.cartItems ? cart.cartItems.find((data) => data.bookName === props.book.bookName) : "";
+    if (book) {
+      return (
+        <>
+          <Button style={{
+            width: '200px',
+            height: '30px',
+            backgroundColor: '#3371B5',
+            color: '#FFFFFF',
+            fontSize: '10px',
+            textTransform: 'none'
+          }}>ADDED TO CART</Button>
+        </>
+      )
+    } else {
+      return (
+        <>
+          <Button style={{
+            width: '90px',
+            height: '30px',
+            backgroundColor: '#A03037',
+            color: '#FFFFFF',
+            fontSize: '10px',
+            textTransform: 'none'
+          }} onClick={addToCart}>ADD TO BAG</Button>
+          <Button style={{
+            border: '1px solid #0A0102',
+            width: '90px',
+            height: '30px',
+            backgroundColor: '#FFFFFF',
+            color: '#0A0102',
+            fontSize: '10px',
+            textTransform: 'none'
+          }}>WISHLIST</Button>
+        </>
+      )
+    }
+  }
+
+  let image = props.book.bookImage ? props.book.bookImage : bookImg
 
   return (<>
-
-    <div className='allBooks'>
+    < div className='allBooks'>
       <div className='book'>
         <div className='bookImg'>
           <img height={'140px'} width={'115px'} src={image} alt='img' />
         </div>
         <div className='book-details'>
-          <span className='book-name'>{props.arrBook.bookName}</span>
-          <span className='author-name'>by {props.arrBook.author}</span>
+          <span className='book-name'>{props.book.bookName}</span>
+          <span className='author-name'>by {props.book.author}</span>
           <div className='book-rate'>
-            <span>Rs. {props.arrBook.price}</span>
+            <span>Rs. {props.book.price}</span>
           </div>
           <div className='review'>
             {
-              addCart ? <>
-                <Button style={{
-                  width: '90px',
-                  height: '30px',
-                  backgroundColor: '#A03037',
-                  color: '#FFFFFF',
-                  fontSize: '10px',
-                  textTransform: 'none'
-                }} onClick={addToCart}>ADD TO BAG</Button>
-                <Button style={{
-                  border: '1px solid #0A0102',
-                  width: '90px',
-                  height: '30px',
-                  backgroundColor: '#FFFFFF',
-                  color: '#0A0102',
-                  fontSize: '10px',
-                  textTransform: 'none'
-                }}>WISHLIST</Button>
-              </>
-                :
-                <>
-                  <Button style={{
-                    width: '200px',
-                    height: '30px',
-                    backgroundColor: '#3371B5',
-                    color: '#FFFFFF',
-                    fontSize: '10px',
-                    textTransform: 'none'
-                  }}>ADDED TO CART</Button>
-                </>
+              button()
             }
             {/* <div className='rating-background'>
                 <span className='rating'>4.5 *</span>
@@ -81,7 +111,7 @@ const DisplayBook = (props) => {
       </div>
     </div>
   </>
-  );
+  )
 }
 
 export default DisplayBook;
