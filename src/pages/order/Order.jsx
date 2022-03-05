0bfd37ab681acd1be20a12c744cdafdcf6e7719e
saranyellanki <saran.yellanki12@gmail.com>
2022-03-05 13:22:32 +0530

    [Saran] Added: order page to include order component and header component

---
+++ src/pages/order/Order.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from '../../components/header/Header';
+import OrderPage from '../../components/orderPage/OrderPage';
+
+const Order = () => {
+  return <div>
+    <Header />
+    <OrderPage />
+  </div>;
+}
+
+export default Order;
