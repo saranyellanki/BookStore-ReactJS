1ef79a6d3c26e6fc0f0c00c1044c4951a6e1afb8
saranyellanki <saran.yellanki12@gmail.com>
2022-03-05 13:15:17 +0530

    [Saran] Refactored: to add customer fields and order summary

--- src/components/displayCart/DisplayCart.jsx
+++ src/components/displayCart/DisplayCart.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './DisplayCart.scss'
 import img from '../../assets/Image 8.png'
-import { Button, TextField, TextareaAutosize, Checkbox } from '@mui/material';
+import { Button, TextField } from '@mui/material';
 import CartService from '../../services/CartService';
 import { AddCircleOutline, RemoveCircleOutline } from '@mui/icons-material';
 import { useSelector } from 'react-redux';
@@ -12,10 +12,28 @@ import RadioGroup from '@mui/material/RadioGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import FormControl from '@mui/material/FormControl';
 import FormLabel from '@mui/material/FormLabel';
+import CustomerService from '../../services/CustomerService';
+import { useNavigate } from 'react-router-dom';
+import OrderService from '../../services/OrderService';
 
+const customerService = new CustomerService();
 const cartService = new CartService();
+const orderService = new OrderService();
 
 const DisplayCart = () => {
+
+  const [customerFields, setcustomerFields] = React.useState({
+    name: "",
+    phoneNumber: "",
+    pincode: "",
+    locality: "",
+    address: "",
+    city: "",
+    landmark: "",
+    addressType: "",
+  })
+
+  const navigate = useNavigate();
 
   const dispatch = useDispatch();
 
@@ -35,6 +53,8 @@ const DisplayCart = () => {
 
   const [customer, setcustomer] = React.useState(false)
 
+  const [order, setorder] = React.useState(false)
+
   const cartArr = useSelector((state) => state.getCart)
 
   const customerDetails = () => {
@@ -45,6 +65,69 @@ const DisplayCart = () => {
     cartService.deleteItem(book._id)
       .then((res) => {
         console.log(res);
+        getCartItems();
+      }).catch((err) => {
+        console.log(err);
+      })
+  }
+
+  const fields = (event) => {
+    setcustomerFields(previousValues => {
+      return { ...previousValues, [event.target.name]: event.target.value }
+    })
+  }
+
+  const saveCustomer = () => {
+    let data = {
+      "addressType": customerFields.addressType,
+      "fullAddress": customerFields.address,
+      "city": customerFields.city,
+      "landmark": customerFields.landmark,
+      "state": customerFields.city,
+      "name": customerFields.name,
+      "phoneNumber": customerFields.phoneNumber,
+      "pincode": customerFields.pincode,
+      "locality": customerFields.pincode
+    }
+    customerService.addCustomer(data)
+      .then((res) => {
+        console.log(res);
+        setorder(true)
+      }).catch((err) => {
+        console.log(err);
+      })
+  }
+
+  const decrement = (book) => {
+    let data = {
+      "_id": book._id,
+      "quantity": book.quantity - 1
+    }
+    cartService.updateCart(data)
+      .then((res) => {
+        getCartItems();
+      }).catch((err) => {
+        console.log(err);
+      })
+  }
+
+  const increment = (book) => {
+    let data = {
+      "_id": book._id,
+      "quantity": book.quantity + 1
+    }
+    cartService.updateCart(data)
+      .then((res) => {
+        getCartItems();
+      }).catch((err) => {
+        console.log(err);
+      })
+  }
+
+  const checkout = () => {
+    orderService.addToOrders()
+      .then((res) => {
+        navigate('/checkout')
         getCartItems();
       }).catch((err) => {
         console.log(err);
@@ -71,13 +154,13 @@ const DisplayCart = () => {
                 </div>
                 <div className="quantity">
                   <div className='decrement'>
-                    <RemoveCircleOutline />
+                    <RemoveCircleOutline onClick={() => decrement(book)} />
                   </div>
                   <div className='book-number'>
-                    <span> 1 </span>
+                    <span> {book.quantity} </span>
                   </div>
                   <div className='increment'>
-                    <AddCircleOutline />
+                    <AddCircleOutline onClick={() => increment(book)} />
                   </div>
                   <div className='delete'>
                     <span onClick={() => removeItem(book)}>Remove</span>
@@ -110,62 +193,69 @@ const DisplayCart = () => {
                 size='small'
                 variant="outlined"
                 style={{ paddingBottom: '20px', paddingRight: '10px', width: '300px' }}
-                InputLabelProps={{ style: { fontSize: 15 } }} />
+                InputLabelProps={{ style: { fontSize: 15 } }}
+                onChange={fields} />
               <TextField
                 id="outlined-basic"
                 label="Phone number"
-                name='email'
+                name='phoneNumber'
                 size='small'
                 variant="outlined"
                 style={{ paddingBottom: '20px', paddingRight: '10px', width: '300px' }}
-                InputLabelProps={{ style: { fontSize: 15 } }} />
+                InputLabelProps={{ style: { fontSize: 15 } }}
+                onChange={fields} />
             </div>
             <div>
               <TextField
                 id="outlined-basic"
                 label="Pincode"
-                name='email'
+                name='pincode'
                 size='small'
                 variant="outlined"
                 style={{ paddingBottom: '20px', paddingRight: '10px', width: '300px' }}
-                InputLabelProps={{ style: { fontSize: 15 } }} />
+                InputLabelProps={{ style: { fontSize: 15 } }}
+                onChange={fields} />
               <TextField
                 id="outlined-basic"
                 label="Locality"
-                name='email'
+                name='locality'
                 size='small'
                 variant="outlined"
                 style={{ paddingBottom: '20px', paddingRight: '10px', width: '300px' }}
-                InputLabelProps={{ style: { fontSize: 15 } }} />
+                InputLabelProps={{ style: { fontSize: 15 } }}
+                onChange={fields} />
             </div>
             <div>
               <TextField
                 id="outlined-basic"
                 label="Address"
                 multiline
-                name='email'
+                name='address'
                 size='large'
                 variant="outlined"
                 style={{ paddingBottom: '10px', width: '610px' }}
-                InputLabelProps={{ style: { fontSize: 15 } }} />
+                InputLabelProps={{ style: { fontSize: 15 } }}
+                onChange={fields} />
             </div>
             <div className='textFields'>
               <TextField
                 id="outlined-basic"
                 label="city/town"
-                name='email'
+                name='city'
                 size='small'
                 variant="outlined"
                 style={{ paddingBottom: '20px', paddingRight: '10px', width: '300px' }}
-                InputLabelProps={{ style: { fontSize: 15 } }} />
+                InputLabelProps={{ style: { fontSize: 15 } }}
+                onChange={fields} />
               <TextField
                 id="outlined-basic"
                 label="Landmark"
-                name='email'
+                name='landmark'
                 size='small'
                 variant="outlined"
                 style={{ paddingBottom: '20px', paddingRight: '10px', width: '300px' }}
-                InputLabelProps={{ style: { fontSize: 15 } }} />
+                InputLabelProps={{ style: { fontSize: 15 } }}
+                onChange={fields} />
             </div>
             <div>
               <FormControl>
@@ -175,13 +265,13 @@ const DisplayCart = () => {
                   aria-labelledby="demo-row-radio-buttons-group-label"
                   name="row-radio-buttons-group"
                 >
-                  <FormControlLabel value="Home" control={<Radio />} label="Home" />
-                  <FormControlLabel value="Work" control={<Radio />} label="Work" />
-                  <FormControlLabel value="Other" control={<Radio />} label="Other" />
+                  <FormControlLabel name='addressType' value="Home" control={<Radio />} label="Home" onChange={fields} />
+                  <FormControlLabel name='addressType' value="Work" control={<Radio />} label="Work" onChange={fields} />
+                  <FormControlLabel name='addressType' value="Other" control={<Radio />} label="Other" onChange={fields} />
                 </RadioGroup>
               </FormControl>
             </div>
-            <div className="place-order">
+            <div className="place-order" onClick={saveCustomer}>
               <Button style={{
                 width: '150px',
                 height: '35px',
@@ -196,6 +286,44 @@ const DisplayCart = () => {
           <div className="customer">
             <span>Customer Details</span>
           </div>
+        }
+        {
+          order ?
+            <div className="order-page">
+              <div className='cart'>
+                <div className="cart-heading">
+                  <span>Order Summary</span>
+                </div>
+                {cartArr.cartItems ?
+                  cartArr.cartItems.map((book, index) =>
+                    <div key={book._id}>
+                      <div className="cart-book">
+                        <img src={img} alt="img" />
+                        <div className="book-detail">
+                          <span className='book-title'>{book.bookName}</span>
+                          <span className='book-author'>by {book.author}</span>
+                          <span className='book-price'>Rs. {book.price}</span>
+                        </div>
+                      </div>
+                    </div>
+                  ) :
+                  ""}
+                <div className="place-order" onClick={checkout}>
+                  <Button style={{
+                    width: '150px',
+                    height: '35px',
+                    backgroundColor: '#3371B5',
+                    color: '#FFFFFF',
+                    fontSize: '13px',
+                    textTransform: 'none'
+                  }}>CHECKOUT</Button>
+                </div>
+              </div>
+            </div>
+            :
+            <div className="customer">
+              <span>Order Summary</span>
+            </div>
         }
       </div>
     </>
