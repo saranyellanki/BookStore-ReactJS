b05e2ff0cff91714d82430a82afb5292e6d610d1
saranyellanki <saran.yellanki12@gmail.com>
2022-03-05 12:56:20 +0530

    [Saran] Refactored: to add order path after successful order

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Homepage from './pages/homepage/Homepage';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Dashboard from './pages/dashboard/Dashboard';
 import Cart from './pages/cart/Cart';
+import Order from './pages/order/Order';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Route path='' element={<Homepage />} />
           <Route path='/dashboard' element={<Dashboard />} />
           <Route path='/cart' element={<Cart />} />
+          <Route path='/checkout' element={<Order />} />
         </Routes>
       </Router>
     </>
