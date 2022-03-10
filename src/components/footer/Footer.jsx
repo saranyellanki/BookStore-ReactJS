bae85736720a595fdea7f9618c05c6b370b61c60
saranyellanki <saran.yellanki12@gmail.com>
2022-03-10 12:45:16 +0530

    [Saran] Added: footer component and set at bottom of the page

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Footer.scss'
+
+const Footer = () => {
+  return <div className='footer'>
+    <footer>Copyright 2020, Bookstore Private Limited. All Rights Reserved</footer>
+  </div>;
+}
+
+export default Footer;
