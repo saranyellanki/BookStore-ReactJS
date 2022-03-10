41c72d7365f6b55e9524c4f7a62eca630c88dc77
saranyellanki <saran.yellanki12@gmail.com>
2022-03-10 12:45:59 +0530

    [Saran] Added: custome pagination hook to set 10 book per page

---
+++ src/components/pagination/Pagination.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+function usePagination(data, dataPerPage) {
+  const [page, setPage] = React.useState(1)
+  const pageCount = Math.ceil(data.length/dataPerPage)
+
+  function currentData() {
+    if(data.length>0) {
+      const start = (page - 1) * dataPerPage;
+      const end = start + dataPerPage;
+      return data.slice(start, end);
+    }
+  }
+
+  function next() {
+    setPage(page => Math.min(page + 1, pageCount));
+  }
+
+  function previous() {
+    setPage(page => Math.min(page - 1, pageCount));
+  }
+
+  function jump(p) {
+    const pageNumber = Math.max(1,p);
+    setPage(page => Math.min(pageNumber, pageCount))
+  }
+
+  return  {next, previous, jump, currentData, page, pageCount} 
+}
+
+export default usePagination;
