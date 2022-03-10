e9baa5f9e37b716e06a852a9d432e69ad5f0b47e
saranyellanki <saran.yellanki12@gmail.com>
2022-03-10 12:43:18 +0530

    [Saran] Refactored: to add footer component in cart page

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,20 +1,29 @@
+import { Pagination } from '@mui/material';
 import React from 'react';
 import DisplayBook from '../../components/displayBook/DisplayBook';
+import Footer from '../../components/footer/Footer';
 import Header from '../../components/header/Header';
+import usePagination from '../../components/pagination/Pagination';
 import BookService from '../../services/BookService'
-import './Dashboard.scss'
+import './Dashboard.scss';
 
 const bookService = new BookService();
 
 const Dashboard = () => {
 
   const [bookArr, setBookArr] = React.useState([]);
+  const [page, setpage] = React.useState(1);
+  const [search,setSearch] = React.useState("");
 
   const getBooks = () => {
     bookService.getAllBooks()
       .then((res) => {
-        // console.log(res);
-        setBookArr(res.data.data)
+        if(search) {
+          let filterBook = res.data.data.filter(book => book.bookName.toLowerCase().includes(search.toLowerCase()))
+          setBookArr(filterBook)
+        } else {
+          setBookArr(res.data.data)
+        }
       }).catch((err) => {
         console.log(err);
       })
@@ -22,18 +31,39 @@ const Dashboard = () => {
 
   React.useEffect(() => {
     getBooks();
-  }, [])
+  }, [search])
+
+  const PER_PAGE = 10;
+
+  var bookArrLength = bookArr ? bookArr.length : 0;
+  const pageCount = Math.ceil(bookArrLength / PER_PAGE)
+  const paginate = usePagination(bookArr, PER_PAGE)
+
+  const changePage = (event,page) => {
+    setpage(page);
+    paginate.jump(page)
+  };
+
+  const searchBook = (value) => {
+    setSearch(value)
+  }
 
   return <div className='bookStore'>
-    <Header />
+    <Header search={searchBook} />
     <div className='heading'>
       <span>Books</span>
     </div>
     <div className='display-books'>
-      {bookArr.length > 0 && bookArr.map((book, index) => (
+      {paginate.currentData() ? paginate.currentData().map((book, index) => (
         <DisplayBook key={index} book={book} getBooks={getBooks} />
-      ))}
+      ))
+        :
+        ""}
     </div>
+    <div className='pagination'>
+      <Pagination count={pageCount} page={page} onChange={changePage} />
+    </div>
+    <Footer />
   </div>;
 }
 
